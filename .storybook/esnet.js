cff231a202691b90c0001cad2a3f7c52fca3a5a6
Peter Murphy <pmurphy@es.net>
2020-03-06 05:33:13 -0800

    Update storybook config to be the same as the charts

---
+++ .storybook/esnet.js
@@ -0,0 +1,18 @@
+import { create } from "@storybook/theming/create";
+
+export default create({
+    base: "light",
+
+    colorPrimary: "gray",
+    colorSecondary: "#4ec1e0",
+
+    // UI
+    appBg: "#fdfdfd",
+    appContentBg: "#ffffff",
+    appBorderColor: "grey",
+    appBorderRadius: 4,
+
+    brandTitle: "react-dynamic-forms",
+    brandUrl: "https://software.es.net",
+    brandImage: "http://software.es.net/react-dynamic-forms/static/media/forms.aec5c7b8.png"
+});
---
+++ .storybook/manager.js
@@ -0,0 +1,6 @@
+import { addons } from "@storybook/addons";
+import theme from "./esnet";
+
+addons.setConfig({
+    theme
+});
